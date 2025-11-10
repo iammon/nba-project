85884dc352d6a88fd78cd83ffc4d7bd2bb320221
EvanCastner <ekc30@uakron.edu>
2025-11-10 17:39:16 +0000

    Edit homepage to add teams lookup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ export default function Home() {
     <main className="mx-auto max-w-3xl p-8 space-y-6">
       <h1 className="text-2xl sm:text-3xl font-bold tracking-tight">NBA Project</h1>
       <p className="text-gray-600">
-        Next.js + PostgreSQL + Prisma. Use the link below to view players from the database.
+        Seacrh for Players or Teams in the NBA.
       </p>
 
-      <a
-        href="/players"
-        className="inline-block rounded-xl px-4 py-2 bg-black text-white hover:bg-gray-800"
-      >
-        View Players
-      </a>
+      <div className="flex flex-col gap-3">
+        <a
+          href="/players"
+          className="inline-block rounded-xl px-4 py-2 bg-black text-white hover:bg-gray-800"
+        >
+          View Players
+        </a>
+        <a
+          href="/teams"
+          className="inline-block rounded-xl px-4 py-2 bg-black text-white hover:bg-gray-800"
+        >
+          View Teams
+        </a>
+      </div>
     </main>
   );
 }
