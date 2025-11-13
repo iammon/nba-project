03f13c0c3604ed0bbd8cc294d54b8ad22bf33bd1
EvanCastner <ekc30@uakron.edu>
2025-11-13 23:32:01 +0000

    Add boxscores to web page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="mx-auto max-w-3xl p-8 space-y-6">
       <h1 className="text-2xl sm:text-3xl font-bold tracking-tight">NBA Project</h1>
       <p className="text-gray-600">
-        Seacrh for Players or Teams in the NBA.
+        Search for Players, Teams, or Boxscores in the NBA.
       </p>
 
       <div className="flex flex-col gap-3">
@@ -20,6 +20,12 @@ export default function Home() {
         >
           View Teams
         </a>
+        <a
+          href="/bocscores"
+          className="inline-block rounded-x1 px-4 py-2 bg-black text-white hover:bg-gray-9=800"
+          >
+            View Boxscores
+         </a>
       </div>
     </main>
   );
